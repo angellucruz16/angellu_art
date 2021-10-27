7335cf0d2a68c88003734cd7611f2f4a8b10b890
angellucruz16 <angellucruz16@hotmail.com>
2021-10-27 02:07:08 -0500

    adding functions to gallery js

--- javaScript/s4-slider/app.js
+++ javaScript/s4-slider/app.js
@@ -1,4 +1,4 @@
-// ✅🌞 product js
+// ✅♍️ product js
 
 const artWorks = [
 {
@@ -41,3 +41,39 @@ const artWorks = [
     description: "Lorem ipsum dolor sit amet consectetur adipiscing elit egestas, mattis vel a dictum elementum risus nisi sem dictumst,"
 }
 ];
+//   🉐no recuerdo como era la relacion entre estos dos elementos
+const productSection = document.getElementById("products");
+
+
+
+// ✅♍️ function that shows the product on screen
+
+const productTemplate = (item) => {
+    // 1. Creating our element to edit html
+    const product = document.createElement("a");
+    product.className ="product";
+    product.innerHTML = `
+    <img
+    src="${item.image}"
+    alt="${item.image}"
+    class="product__img"
+  />
+  <div class="product__description">
+    <h2 class="product__available--sold-out">Sold out</h2>
+    <span class="product__tag paragraph" id="description"
+      >collection</span
+    >
+    <h3 class="product__title title" id="description">${item.title}</h3>
+    <button>
+      <i
+        class="product__cart-btn bx-border bx bx-cart bx-burst-hover"
+        id="description"
+        style="color: #f2b30f"
+      ></i>
+    </button>
+  </div>
+  `;
+//   🉐no recuerdo como era la relacion entre estos dos elementos
+  productSection.appendChild(product);
+
+}

d18deb4837b1f330bdfdf837a0c1eecfa1da2eae
angellucruz16 <angellucruz16@hotmail.com>
2021-10-27 01:46:00 -0500

    starting product js logic

---
+++ javaScript/s4-slider/app.js
@@ -0,0 +1,43 @@
+// ✅🌞 product js
+
+const artWorks = [
+{
+    title: "title",
+    price: "40,99",
+    image: "../img/s3-products/s3-img.png",
+    type: "print",
+    isBestSeller: true,
+    collection: true,
+    inStock: true,
+    isAdded: false,
+    size: "13x16",
+    quantity: "1",
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit egestas, mattis vel a dictum elementum risus nisi sem dictumst,"
+},
+{
+    title: "title",
+    price: "40,99",
+    image: "../img/s3-products/s3-img.png",
+    type: "sticker",
+    isBestSeller: false,
+    collection: false,
+    inStock: true,
+    isAdded: false,
+    size: "13x16",
+    quantity: "1",
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit egestas, mattis vel a dictum elementum risus nisi sem dictumst,"
+},
+{
+    title: "title",
+    price: "40,99",
+    image: "../img/s3-products/s3-img.png",
+    type: "merch",
+    isBestSeller: true,
+    collection: true,
+    inStock: false,
+    isAdded: false,
+    size: "13x16",
+    quantity: "1",
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit egestas, mattis vel a dictum elementum risus nisi sem dictumst,"
+}
+];

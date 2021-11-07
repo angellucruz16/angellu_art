79e7075f1d7ab0c61ac8a1208266873bca7b3add
angellucruz16 <angellucruz16@hotmail.com>
2021-11-07 17:39:48 -0500

    starting product detail logic

---
+++ product-detail/javaScript/product.js
@@ -0,0 +1,24 @@
+// ✅♍️ product Detail js
+        // 🍓 to get the id
+const url = window.location.search;
+var searchParams = new URLSearchParams(url);
+        // 🍓 save id in a variable
+const productId = searchParams.get("id");
+
+const product = products.find(product => product.id == productId);
+console.log(product);
+
+// ✅♍️ Pov Guide
+const povSection = document.querySelector(".pov");
+const povTemplate = (item) => {
+        const pov = document.createElement ("a");
+        pov.innerHTML = `
+        <p>artwork</p>
+        <img src="./img/s1-header/pov-arrow.svg" alt="pov arrow" />
+        <p class="pov__art-name"> ${item.title}</p>
+        `;
+  povSection.appendChild(pov);
+  console.log("hola");
+};
+
+povTemplate(product); 

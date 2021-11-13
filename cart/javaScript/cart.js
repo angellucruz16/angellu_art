c227912a6e26179060f78c732858ab9983abc17b
angellucruz16 <angellucruz16@hotmail.com>
2021-11-12 22:16:38 -0500

    adding css to cart

--- cart/javaScript/cart.js
+++ cart/javaScript/cart.js
@@ -24,36 +24,39 @@ const renderProduct =  (product) => {
     src="${product.image}"
     alt="${product.image}"
   />
-  <div class="product-view__template__info--title">
-    <p class="title-tag">${product.title}</p>
-    <p>size</p>
-  </div>
-  <div class="product-view__template__info--price">
-    <p class="title-tag">Price</p>
-    <p>${product.price}/p>
-  </div>
-  <div class="product-view__template__info--Quantity">
-    <p class="title-tag">Quantity</p>
-    <select name="quantity" id="quantity">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-    </select>
-  </div>
-  <div class="product-view__template--total">
-    <p class="title-tag">Total</p>
-    <p>$60000</p>
-  </div>
-  <button class="product-view__template--btn" id="button-delete">
-    <img src="./img/s2-cart-product-view/delete-button.svg" alt="" />
-  </button>`;
+  <div class="product-view__template__info__data">
+    <div class="product-view__template__info__title">
+      <p class="title-tag">${product.title}</p>
+      <p>size</p>
+    </div>
+    <div class="product-view__template__info__price">
+      <p class="title-tag">Price</p>
+      <p>${product.price}/p>
+    </div>
+    <div class="product-view__template__info__Quantity">
+      <p class="title-tag">Quantity</p>
+      <select name="quantity" id="quantity">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+    <div class="product-view__template__total">
+      <p class="title-tag">Total</p>
+      <p>$60000</p>
+    </div>
+    <button class="product-view__template--btn" id="button-delete">
+      <img src="./img/s2-cart-product-view/delete-button.svg" alt="" />
+    </button>
+  </div>`;
+  
 
   
     const deteleBtn = newProdct.querySelector("#button-delete");

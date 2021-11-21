36b315aa503cec8ba0ecf4d0b4e0fbe33b45bc1f
angellucruz16 <angellucruz16@hotmail.com>
2021-11-21 18:19:08 -0500

    adding header on cart and gallery

--- utils.js
+++ utils.js
@@ -17,3 +17,15 @@ const formatCurrency = (price) => {
     }).format(price);
    };
 
+//header
+const navMenu = document.getElementById("nav-menu");
+const toggleMenu = document.getElementById("toggle-menu");
+const closeMenu = document.getElementById("close-menu");
+
+toggleMenu.addEventListener("click",() => {
+    navMenu.classList.toggle("show");
+    console.log("click");
+});
+closeMenu.addEventListener("click",() => {
+    navMenu.classList.remove("show");
+});
